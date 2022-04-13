16ceffa965686d494f11fc4c663efb5118c68da0
danielsollus <danielsuporteti@outlook.com.br>
2022-04-13 13:05:40 -0300

    comentario na SecretKey

--- TarefasBackEnd/SecretCompartilhada.cs
+++ TarefasBackEnd/SecretCompartilhada.cs
@@ -9,16 +9,17 @@ namespace TarefasBackEnd
     {
         public static string Secret
         {
-            get
-            {
-                string path = Path.Combine(Environment.CurrentDirectory, @"Secret_Key.txt").ToString();
-                var CurrentDirectory = Environment.CurrentDirectory;
+            get;
+            //{
+            //    string path = Path.Combine(Environment.CurrentDirectory, @"Secret_Key.txt").ToString();
+            //    var CurrentDirectory = Environment.CurrentDirectory;
 
-                using (StreamReader streamReader = File.OpenText(path))
-                {
-                    return streamReader.ReadLine();
-                }
-            }
-        }
+            //    using (StreamReader streamReader = File.OpenText(path))
+            //    {
+            //        return streamReader.ReadLine();
+            //    }
+            //}
+            set;
+        } = "6c018273-2d63-4e41-b036-1af6c30dbaa3";
     }
 }
