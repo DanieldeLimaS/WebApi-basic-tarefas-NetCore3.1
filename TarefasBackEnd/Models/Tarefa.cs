b7b4a46ccbf8597eb37124e0f9555b11841dd692
danielsollus <danielsuporteti@outlook.com.br>
2022-04-16 19:12:11 -0300

    Configurando as Models para interagir com o banco de dados

--- TarefasBackEnd/Models/Tarefa.cs
+++ TarefasBackEnd/Models/Tarefa.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TarefasBackEnd.Models
 {
     public class Tarefa
     {
+        [Column("id")]
         public Guid Id { get; set; }
+        [Column("usuarioId")]
         public Guid UsuarioId { get; set; }
         [Required]
+        [Column("nome")]
         public string Nome { get; set; }
+        [Column("concluida")]
         public bool Concluida { get; set; }
     }
 }
--- TarefasBackEnd/Models/Usuario.cs
+++ TarefasBackEnd/Models/Usuario.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TarefasBackEnd.Models
 {
+    [Table("usuario")]
     public class Usuario
     {
+        [Column("id")]
         public Guid Id { get; set; }
+        [Column("nome")]
         public string Nome { get; set; }
+        [Column("email")]
         public string Email { get; set; }
+        [Column("senha")]
         public string Senha { get; set; }
     }
 }
